22fe85da977ad3920b617e56ac5c64e08f7487b7
Qamrosh <qamroshmaqsood9@gmail.com>
2024-05-07 17:13:25 +0500

    Updated

--- src/PriceRange.jsx
+++ src/PriceRange.jsx
@@ -1,42 +1,3 @@
-// import { Slider } from "@nextui-org/react";
-// import React from "react";
-// import { useTextareaContext } from './TextareaProvider';
-
-// export default function PriceRange() {
-//   const { priceRange, setPriceRange } = useTextareaContext(); // Use context
-  
-//   const handleSliderChange = (value) => {
-//     const hyphenatedRange = value.join("-"); // Convert to hyphenated string
-//     setPriceRange(hyphenatedRange); // Store as string in context
-//   };
-
-//   // Split the hyphenated string and convert to array of numbers
-//   const rangeArray = priceRange.split("-").map(Number); // Safe split
-
-//   return (
-//     <div className="flex flex-col gap-2 w-full h-full max-w-md items-start justify-center">
-//       <Slider 
-//         label="Select a budget"
-//         formatOptions={{style: "currency", currency: "PKR"}}
-//         step={10}
-//         maxValue={5000}
-//         minValue={300}
-//         value={rangeArray} // Use the converted array
-//         onChange={handleSliderChange} // Use the hyphenated conversion
-//         className="max-w-md"
-//       />
-//       <p className="text-default-500 font-medium text-small">
-//         Selected budget: {priceRange} 
-//       </p>
-//     </div>
-//   );
-// }
-
-
-
-
-
-
 import { Slider } from "@nextui-org/react";
 import React from "react";
 import { useTextareaContext } from './TextareaProvider';
@@ -60,8 +21,8 @@ export default function PriceRange() {
         step={10}
         maxValue={5000}
         minValue={300}
-        value={[rangeValue]} // Pass single value as an array
-        onChange={handleSliderChange} // Use the modified function
+        value={[rangeValue]} 
+        onChange={handleSliderChange} 
         className="max-w-md"
       />
       <p className="text-default-500 font-medium text-small">
